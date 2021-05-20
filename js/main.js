5d47da381a3a7df974f907a1e41f1d12ec78387b
Weronika Klich <klichweronika9@gmail.com>
2021-05-20 13:55:44 +0200

    Feat(js, smoothie-delight): Add menu icon, change toggleMenu and stickHeaderToTheTop function, fix styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
-function headerListener() {
-    window.addEventListener("scroll", function () {
-        var header = document.querySelector("header");
-        header.classList.toggle("sticky", window.scrollY > 0);
+function stickHeaderToTheTop() {
+    window.addEventListener("scroll", () => {
+        const classList = document.querySelector("header").classList;
+        classList.toggle("sticky", window.scrollY > 0);
       });
     }
+
+function toggleMenu() {
+    var menuToggle = document.querySelector(".toggle");
+    var menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+  }
